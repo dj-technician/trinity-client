42415f5fe7686d0bb9e11d390f52ab3536e11527
devjk <rmsghk4254@naver.com>
2023-09-12 22:44:00 +0900

    feat(front): tailwind typography 추가

--- trinity-front/pages/[userName]/index.vue
+++ trinity-front/pages/[userName]/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="flex flex-col bg-stone-800 h-full p-3 select-auto">
     <div class="flex w-full h-full justify-center place-items-center text-lime-200 text-6xl">
-      This is Home page
+      <article class="prose prose-stone">
+        {{markdown}}
+      </article>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+const markdown = "# Hello World"
 </script>
 
 <style scoped>
