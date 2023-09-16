7f5b0a88ac1010041ee53010d99708609a4408f2
devjk <rmsghk4254@naver.com>
2023-09-16 18:20:37 +0900

    feat(front): ci/cd test

--- pages/[userName]/about.vue
+++ pages/[userName]/about.vue
@@ -2,6 +2,7 @@
   <div class="flex flex-col bg-stone-800 h-full p-3 select-auto">
     <div class="flex w-full h-full justify-center place-items-center text-lime-200 text-6xl">
       WE'RE PREPARING THIS PAGE
+      THIS IS ABOUT PAGE
     </div>
   </div>
 </template>
